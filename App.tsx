c16d2e2c18fcd2f8b9c081a86b9950cf91122c36
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-04-24 15:23:39 +0530

    Respect Device Screen Restrictions with the SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import StartGameScreen from 'screens/StartGameScreen';
 import { ReactNode, useState } from 'react';
@@ -27,7 +27,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
