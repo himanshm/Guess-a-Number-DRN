3bb45a9fe6cafc0e34a1e0ceba7c6cf53ac910a8
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-04-24 12:30:54 +0530

    add styles

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,10 +1,10 @@
-import { TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 import PrimaryButton from 'components/PrimaryButton';
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -13,3 +13,18 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#6C0345',
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: '#0C0C0C',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
