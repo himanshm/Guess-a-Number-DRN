7c23e541a9d474a00f2a3369f6157c341b036597
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-04-25 15:30:19 +0530

    Adding a gameover screen

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,7 +1,43 @@
-import { Text } from 'react-native';
+import Title from 'components/ui/Title';
+import Colors from 'constants/colors';
+import { Image, View, StyleSheet, Text } from 'react-native';
 
 function GameOverScreen() {
-  return <Text>GameOverScreen</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/images/success.png')}
+          style={styles.image}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
